30cc1e4e1679333633bc97a33144470817e0ea45
Jason Lo <lcmjlo@gmail.com>
2025-06-30 16:14:57 -0500

    delete early experimental s3 setup

--- pelican_loader/utils.py
+++
@@ -1,7 +0,0 @@
-import string
-
-
-def sanitize_name(name: str) -> str:
-    """Sanitize a string to be a valid filename by replacing invalid characters with '_'."""
-    valid_chars = string.ascii_letters + string.digits + "-_."
-    return "".join(c if c in valid_chars else "_" for c in name)
